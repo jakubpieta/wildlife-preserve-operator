81200db3b436909e3bd7b1aed1c53e16995a9f7d
Jakub Pięta <jakub.pieta@nobl9.com>
2023-10-05 13:20:53 +0200

    first commit

---
+++ api/v1alpha1/wildlifepreserve_types.go
@@ -0,0 +1,63 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// WildlifePreserveSpec defines the desired state of WildlifePreserve
+type WildlifePreserveSpec struct {
+	Name     string `json:"name"`
+	Location string `json:"location"`
+	Replicas int32  `json:"replicas"`
+	Volume   string `json:"volume"`
+}
+
+// WildlifePreserveStatus defines the observed state of WildlifePreserve
+type WildlifePreserveStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// WildlifePreserve is the Schema for the wildlife-preserves API
+type WildlifePreserve struct {
+	metaV1.TypeMeta   `json:",inline"`
+	metaV1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   WildlifePreserveSpec   `json:"spec,omitempty"`
+	Status WildlifePreserveStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// WildlifePreserveList contains a list of WildlifePreserve
+type WildlifePreserveList struct {
+	metaV1.TypeMeta `json:",inline"`
+	metaV1.ListMeta `json:"metadata,omitempty"`
+	Items           []WildlifePreserve `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&WildlifePreserve{}, &WildlifePreserveList{})
+}
---
+++ controllers/wildlifepreserve_controller.go
@@ -0,0 +1,124 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+
+	appsV1 "k8s.io/api/apps/v1"
+	coreV1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
+	wildlifeV1 "github.com/jakubpieta/wildlife-preserve-operator/api/v1alpha1"
+)
+
+// WildlifePreserveReconciler reconciles a WildlifePreserve object
+type WildlifePreserveReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=wildlife.preserves.my.domain,resources=wildlifepreserves,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=wildlife.preserves.my.domain,resources=wildlifepreserves/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=wildlife.preserves.my.domain,resources=wildlifepreserves/finalizers,verbs=update
+
+// Reconcile handles WildlifePreserve resource reconciliation
+func (r *WildlifePreserveReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	// Fetch the WildlifePreserve resource
+	preserve := &wildlifeV1.WildlifePreserve{}
+	if err := r.Get(ctx, req.NamespacedName, preserve); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// Handle the volume creation based on the `Volume` field
+	if preserve.Spec.Volume != "" {
+		// Create a volume with the specified configuration
+		// You can use Kubernetes client (client.Client) to create the volume (e.g., PVC)
+		// Example: Create a PVC (PersistentVolumeClaim)
+		pvc := &coreV1.PersistentVolumeClaim{
+			ObjectMeta: metaV1.ObjectMeta{
+				Name: preserve.Name + "-volume",
+			},
+			Spec: coreV1.PersistentVolumeClaimSpec{
+				AccessModes: []coreV1.PersistentVolumeAccessMode{coreV1.ReadWriteOnce},
+				// Add other volume configuration as needed
+			},
+		}
+		if err := r.Create(ctx, pvc); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	// Create or update a Deployment for the Go application with volume mount
+	deployment := &appsV1.Deployment{
+		ObjectMeta: metaV1.ObjectMeta{
+			Name:      preserve.Name + "-deployment",
+			Namespace: req.Namespace,
+		},
+		Spec: appsV1.DeploymentSpec{
+			Replicas: &preserve.Spec.Replicas, // Use the number of replicas from the CR
+			Selector: &metaV1.LabelSelector{
+				MatchLabels: map[string]string{"app": preserve.Name},
+			},
+			Template: coreV1.PodTemplateSpec{
+				ObjectMeta: metaV1.ObjectMeta{
+					Labels: map[string]string{"app": preserve.Name},
+				},
+				Spec: coreV1.PodSpec{
+					Containers: []coreV1.Container{
+						{
+							Name:  "wildlife-preserve-app",
+							Image: "wildlifepreserve", // Replace with your Go app image
+							VolumeMounts: []coreV1.VolumeMount{
+								{
+									Name:      "animals-storage", // Replace with your volume name
+									MountPath: "/animals-storage",
+								},
+							},
+							// Add other container configuration as needed
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// Create or update the Deployment
+	if err := controllerutil.SetControllerReference(preserve, deployment, r.Scheme); err != nil {
+		return ctrl.Result{}, err
+	}
+	err := r.Create(ctx, deployment)
+	if err != nil && !errors.IsAlreadyExists(err) {
+		return ctrl.Result{}, err
+	}
+
+	// Handle other aspects of the WildlifePreserve resource
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *WildlifePreserveReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&wildlifeV1.WildlifePreserve{}).
+		Complete(r)
+}
